787023834487dacf8ea2d0dfef18165ea0ee24b4
ra72e6 <ra72e6@hanmail.net>
2024-06-12 10:10:40 +0900

    0612

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,11 +4,21 @@ import React from 'react';
 export default function Navbar() {
   return (
     <nav>
-      <Link href="/">Blog</Link>
-      <Link href="/about">About</Link>
-      <Link href="/portfolio">포트폴리오</Link>
-      <Link href="/project">프로젝트</Link>
-      <Link href="/contact">연락처</Link>
+      <Link href="/" className="text-2xl font-bold">
+        Blog
+      </Link>
+      <Link href="/about" className="hover-font-bold hover:text-gray-300">
+        About
+      </Link>
+      <Link href="/portfolio" className="hover-font-bold hover:text-gray-300">
+        포트폴리오
+      </Link>
+      <Link href="/project" className="hover-font-bold hover:text-gray-300">
+        프로젝트
+      </Link>
+      <Link href="/contact" className="hover-font-bold hover:text-gray-300">
+        연락처
+      </Link>
     </nav>
   );
 }
